189bffeb4353af6b9bb365583182e2585dd0cb60
Kaleb Ercanbrack <twitcherc@gmail.com>
2021-01-16 15:33:38 -0700

    thicc borders

--- components/ListItem.vue
+++ components/ListItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     @click="$router.push(to)"
-    class="bg-gray-800 border border-gray-700 py-2 rounded cursor-pointer hover:border-gray-600 hover:bg-gray-700 transition-colors duration-75 ease-in"
+    class="bg-gray-800 border-2 border-gray-700 py-2 rounded cursor-pointer hover:border-gray-600 hover:bg-gray-700 transition-colors duration-75 ease-in"
   >
     <div class="flex">
       <slot name="icon"> </slot>
@@ -13,16 +13,16 @@
 </template>
 <script>
 export default {
-  name: "ListItem",
+  name: 'ListItem',
   props: {
     name: {
       type: String,
-      default: "",
+      default: '',
     },
     to: {
       type: String,
-      default: "",
+      default: '',
     },
   },
-};
+}
 </script>
